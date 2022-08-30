90ebc62147fb93a13e2499191c4e4897bfac17ec
Liu Zhang <liu.zhang@theinformationlab.co.uk>
2022-08-30 16:57:14 +0100

    Update FF4_LZ.sql

--- FF4_LZ.sql
+++ FF4_LZ.sql
@@ -1,84 +1,200 @@
 -- set environment
-create schema challenge_4;
-use schema challenge_4;
+create schema challenge_6;
+use schema challenge_6;
 
--- create the stage for loading
+-- Due to colume in csv contains ', need to create a separete file format to load
 create
-    or replace stage challenge_4_AWS_stage url = 's3://frostyfridaychallenges/challenge_4/';
--- check the files in the staging location
-list @challenge_4_AWS_stage;
-
--- create the initial table to load json file in as variant
+    or replace file format ff6_csv type = csv,
+skip_header = 0,
+field_optionally_enclosed_by = '"';
+-- Create loading stage
+create
+    or replace stage challenge_6_AWS_stage url = 's3://frostyfridaychallenges/challenge_6/' file_format = ff6_csv;
+-- Not required: Check the files in the staging location
+list @challenge_6_AWS_stage;
+-- Not required: Checking out the data in the file
+select
+    metadata$filename,
+    metadata$file_row_number,
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+from
+    @challenge_6_AWS_stage
+where
+    metadata$filename = 'challenge_6/westminster_constituency_points.csv';
+-- Create table for loading data
+create
+    or replace table ff6_westminster_constituency_points (
+        constituency string (100),
+        sequence_num int,
+        longitude double,
+        latitude double,
+        part int
+    );
+-- Add values into the table
+insert into
+    ff6_westminster_constituency_points (
+    select
+        $1,
+        $2,
+        $3,
+        $4,
+        $5
+    from
+        @challenge_6_AWS_stage
+    where
+        metadata$filename = 'challenge_6/westminster_constituency_points.csv'
+        and METADATA$file_row_number != 1 // skip the header row
+    );
+-- The approach that I used is to create string that list the coordinates, then cast it as a line, then polygon
+create
+    or replace temp table westminster_point as (
+    select
+        constituency,
+        part,
+        sequence_num,
+        listagg(longitude || ' ' || latitude) as str_point -- create string that concat coordinates
+    from
+        ff6_westminster_constituency_points
+    group by
+        constituency,
+        part,
+        sequence_num
+    );
+-- From above, create line from the points created, then polygon
+create
+    or replace table westminster_polygon as (
+    select
+        constituency,
+        part,
+        listagg(str_point, ', ') within group (order by sequence_num) as str_line -- create line that concat all points
+        ,
+        'LINESTRING(' || str_line || ')' as str_line_2 -- attach LINESTRING as keyword for spatial function
+        ,
+        st_makepolygon(to_geography(str_line_2)) as polygon -- create polygon
+    from
+        westminster_point
+    group by
+        constituency,
+        part
+    );
+-- The field Part is used to make creating polygon easier, now remove it to aggragte the polygons
+create
+    or replace table westminster_combined as (
+    select
+        constituency as constituency,
+        st_collect(polygon) as polygon -- aggragte the polygon field
+    from
+        westminster_polygon
+    group by
+        constituency
+    );
+-- Repeat as above for nations and regions data
+-- Not required: used for checking
+select
+    metadata$filename,
+    metadata$file_row_number,
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+from
+    @challenge_6_AWS_stage
+where
+    metadata$filename = 'challenge_6/nations_and_regions.csv';
 -- create table
 create
-    or replace table ff4_raw_json (file variant);
--- load the data
-copy into ff4_raw_json
-    from @challenge_4_AWS_stage FILE_FORMAT = (TYPE = JSON);
-
--- check the loaded file
-select * from ff4_raw_json;
-    
--- 1st level of parsing
--- save to a level 1 table
-create
-    or replace table ff4_level1 as (
-        -- select the key value pair, if value is not nested then cast to string, else keep for next level parsing
-        select
-            t1.value:Era::string as Era,
-            t1.value:Houses as Houses_l1
-        from
-            ff4_raw_json,
-            lateral flatten(file) as t1
+    or replace table ff6_nations_and_regions_points (
+        nation_or_region_name string,
+        type string,
+        sequence_num int,
+        longitude double,
+        latitude double,
+        part int
     );
-
--- 2nd level of parsing
--- save to a level 2 table
-create
-    or replace table ff4_level2 as (
-        select
-            era,
-            t2.value:House::string as House,
-            t2.value:Monarchs as Monarchs_l2
-        from
-            ff4_level1,
-            lateral flatten(houses_l1) as t2
-    );
-    
-select * from ff4_level2;
-
--- 3nd level of parsing
--- save to a level 3 table
-create
-    or replace table ff4_level3 as (
-        select
-            t3.index+1::int Inter_House_ID, // use index from lateral flatten
-            era,
-            house,
-            split(t3.value:"Age at Time of Death",' ' )[0]::int as Age_at_Time_of_Death,
-            t3.value:"Birth"::date as Birth,
-            t3.value:"Burial Place" ::string as Burial_Place,
-            t3.value:"Consort\/Queen Consort" [0] ::string as Consort_or_Queen_Consort_1,
-            t3.value:"Consort\/Queen Consort" [1] ::string as Consort_or_Queen_Consort_2,
-            t3.value:"Consort\/Queen Consort" [2] ::string as Consort_or_Queen_Consort_3,
-            t3.value:"Death"::date as Death,
-            t3.value:"Duration"::string as Duration,
-            t3.value: "End of Reign" ::date as End_of_Reign,
-            t3.value:"Name"::string as Name,
-            t3.value:"Nickname"[0]::string as Nickname_1,
-            t3.value:"Nickname"[1]::string as Nickname_2,
-            t3.value:"Nickname"[2]::string as Nickname_3,
-            t3.value:"Place of Birth" ::string Place_of_Birth,
-            t3.value:"Place of Death" ::string Place_of_Death,
-            t3.value: "Start of Reign" ::date Start_of_Reign
-        from
-            ff4_level2,
-            lateral flatten(monarchs_l2) as t3
-    );
-
--- Produce the result table
-select
-    ROW_NUMBER() OVER (ORDER BY BIRTH) as ID,
-    *
+-- load table
+insert into
+    ff6_nations_and_regions_points (
+    select
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6
     from
-        ff4_level3 as t;
+        @challenge_6_AWS_stage
+    where
+                metadata$filename = 'challenge_6/nations_and_regions.csv'
+        and METADATA$file_row_number != 1 -- skip the header row
+    );
+-- create points
+create
+    or replace temp table nations_and_regions_point as (
+    select
+        nation_or_region_name,
+        type,
+        part,
+        sequence_num,
+        listagg(longitude || ' ' || latitude) as str_point
+    from
+        ff6_nations_and_regions_points
+    group by
+        nation_or_region_name,
+        type,
+        part,
+        sequence_num
+    );
+-- create polygon
+create
+    or replace table nations_and_regions_polygon as (
+    select
+        nation_or_region_name,
+        type,
+        part,
+        listagg(str_point, ', ') within group (order by sequence_num) as str_line,
+        'LINESTRING(' || str_line || ')' as str_line_2,
+        st_makepolygon(to_geography(str_line_2)) as polygon
+    from
+        nations_and_regions_point
+    group by
+                nation_or_region_name,
+                type,
+                part
+    );
+-- aggregate
+create
+    or replace table nations_and_regions_combined as
+(
+        select
+    nation_or_region_name as name,
+    st_collect(polygon) as polygon
+from
+    nations_and_regions_polygon
+group by
+            nation_or_region_name,
+            type
+    );
+-- Use cross join to append all combinations from two tables, then use spatial intersection to filter out not needed.
+select
+    n.name as NATION_OR_REGION,
+    count(1) as INTERSECTING_CONSITUENCIES
+from
+    WESTMINSTER_COMBINED w
+    cross join nations_and_regions_combined n
+where
+    st_intersects(w.polygon, n.polygon)
+group by
+    n.name
+order by
+    INTERSECTING_CONSITUENCIES desc;
